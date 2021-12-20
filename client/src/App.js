f0d5157eb323325a34306a9b7dfc426a3ed79a44
Jorge Herrera <jorgeherrera@jorges-air.attlocal.net>
2021-12-20 00:39:01 -0600

    all styling done

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,64 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { baseURL } from './services/services';
-import Route from 'react-router-dom';
+import './App.css'
+import { Icon } from '@iconify/react';
 function App() {
-  const [user, setUser] = useState(null)
-
+  const [user, setUser] = useState('')
+  const [userData, setUserData] = useState(null)
 
 
   const handleSubmit = async (e) => {
+
     e.preventDefault()
+    const names = {
+      user
+    }
+
+
+
+    try {
+      const resp = await axios.get(`${baseURL}/${names.user}`)
+      setUserData(resp.data)
+    } catch (error) {
+      window.alert('No match found!')
+    }
+
+
+
+
+
   }
 
 
-  const handleUser = async (userName) => {
 
-    const resp = await axios.get(`${baseURL}/${userName}`)
-
-    setUser(resp.data)
-    console.log(user.name)
-  }
 
 
 
 
   return (
     <div className='main-div'>
-      <>
+      <h1 className='title'>Welcome to Github Finder</h1>
+      <div className='container'>
+        <div className='container-2' >
 
-        <form className='form-sub' onSubmit={handleSubmit}>
-          <label className='input-label'>Please type in the GitHub User:</label>
-          <input className='userName-input' type='text' onChange={(e) => handleUser(e.target.value)} />
-          <button type='submit'>Submit</button>
-        </form>
-        <div>
-          <h1>{user?.name}</h1>
-          <p>{user?.bio}</p>
-          <h2>{user?.login}</h2>
-          <h3>{user?.email}</h3>
-          <img src={user?.avatar_url} />
+          <form className='form-sub' onSubmit={handleSubmit}>
+            <label className='input-label'>Please type in the GitHub User:</label>
+            <input className='userName-input' type='text' onChange={(e) => setUser(e.target.value.toLowerCase())} />
+            <button className='submit-button' type='submit'>Submit</button>
+          </form>
+          <div className='user-info' >
+
+            <h1>Name: {userData?.name}</h1>
+            <p className='bio'>Bio: {userData?.bio}</p>
+            <h2>Login: {userData?.login}</h2>
+            <h3>Email: {userData?.email}</h3>
+            <img className='git-img' src={userData?.avatar_url} />
+          </div>
         </div>
 
 
-
-      </>
+      </div>
     </div>
   );
 
